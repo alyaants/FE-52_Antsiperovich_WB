5c10092f7bedf33e8e8b94fa6fc460caf5582bbc
Alya Antsiperovich <antsiperovich2002@mail.ru>
2023-11-01 23:13:55 +0300

    cart

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,110 @@
 import "./slider";
 
+
+// создание окна корзины
+const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+
+let isCartOpen = false; // состояние корзины
+
+const cartButton = document.querySelector("#cart");
+const overlay = document.createElement("div");
+overlay.className = "overlay";
+document.body.appendChild(overlay);
+const cartWrap = document.createElement("div");
+cartWrap.classList.add("cart-wrap");
+document.body.appendChild(cartWrap);
+
+const cartHeader = document.createElement("div");
+cartHeader.classList.add("cartHeader");
+cartWrap.appendChild(cartHeader);
+
+const titleCart = document.createElement("span");
+titleCart.classList.add("titleCart");
+titleCart.textContent = "Корзина";
+cartHeader.appendChild(titleCart);
+
+const closeButton = document.createElement("button");
+closeButton.classList.add("closeButton");
+closeButton.innerText = "✖";
+cartHeader.appendChild(closeButton);
+
+const listWrap = document.createElement("div");
+listWrap.classList.add("listWrap");
+cartWrap.appendChild(listWrap);
+
+const cartList = document.createElement("ul");
+cartList.className = "listUl";
+listWrap.appendChild(cartList);
+
+const total = document.createElement("span");
+total.classList.add("total");
+total.innerText = "Итого: ";
+listWrap.appendChild(total);
+
+cartButton.addEventListener("click", () => {
+  if (isCartOpen) {
+    cartWrap.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    cartWrap.style.display = "block";
+    overlay.style.display = "block";
+    displayCart();
+  }
+  isCartOpen = !isCartOpen;
+});
+
+closeButton.addEventListener("click", () => {
+  cartWrap.style.display = "none";
+  overlay.style.display = "none";
+  isCartOpen = false;
+});
+
+function displayCart() {
+  const cartData = JSON.parse(localStorage.getItem("cart"));
+  const cartList = document.querySelector(".listUl");
+
+  cartList.innerHTML = "";
+  let totalAmount = 0;
+
+  if (cartData && cartData.length > 0) {
+    cartData.forEach((item) => {
+      const cartItem = document.createElement("li");
+      cartItem.textContent = `${item.name} - ${item.price} BYN`;
+
+      const removeBtn = document.createElement("button");
+      removeBtn.classList.add("removeBtn");
+      removeBtn.textContent = "Удалить";
+
+      removeBtn.addEventListener("click", () => {
+        const index = cartData.findIndex((cartItem) => cartItem.id === item.id);
+        if (index !== -1) {
+          cartData.splice(index, 1);
+          localStorage.setItem("cart", JSON.stringify(cartData));
+          displayCart();
+        }
+      });
+
+      cartItem.appendChild(removeBtn);
+      cartList.appendChild(cartItem);
+
+      totalAmount += item.price;
+    });
+
+    const total = document.querySelector(".total");
+    total.textContent = `Итого: ${totalAmount} BYN`;
+  } else {
+    const emptyCartMessage = document.createElement("p");
+    emptyCartMessage.textContent = "Корзина пуста";
+    cartList.appendChild(emptyCartMessage);
+
+    // обнуление суммы, если корзина пуста
+    const total = document.querySelector(".total");
+    total.textContent = "Итого: 0 BYN";
+  }
+}
+displayCart();
+
+// создание карточек товаров
 async function createProductCards() {
   const response = await fetch(
     "https://653b9efd2e42fd0d54d56a5c.mockapi.io/cards"
@@ -63,21 +168,18 @@ async function createProductCards() {
     firmElement.id = "productFirm";
 
     const addButton = document.createElement("button");
-    addButton.classList.add('addButton')
+    addButton.classList.add("addButton");
     addButton.textContent = "В корзину";
     addButton.addEventListener("click", () => {
-      // Создаем объект для товара, который добавляем в корзину
       const item = {
-        id: card.id, // Уникальный идентификатор товара
+        id: card.id,
         name: card.name,
         price: card.price,
         img: card.img,
       };
-    
-      // Добавляем товар в корзину
+
       cart.push(item);
-    
-      // Сохраняем корзину в локальном хранилище (localStorage)
+
       localStorage.setItem("cart", JSON.stringify(cart));
     });
 
@@ -113,12 +215,13 @@ async function createProductCards() {
           <span id="modalPriceNew" class="prodPrice"></span>
         </div>
         <div class="buttons">
-          <button id="modalAdd" class="addCart"></button>
           <button id="modalMore" class="button-more"></button>
+          <button id="modalLike" class="like"></button>
         </div>
       </div>
     </div>
   `;
+
   document.body.appendChild(viewWindow);
   viewWindow.appendChild(btnWrap);
   btnWrap.appendChild(btnClose);
@@ -138,10 +241,24 @@ async function createProductCards() {
     modalPriceNew.textContent = `${cardData.price} BYN`;
     modalFirmElement.textContent = `${cardData.firm}`;
 
-    const modalAdd = document.getElementById("modalAdd");
     const modalMore = document.getElementById("modalMore");
-    modalAdd.innerHTML = "В корзину";
+    const modalLike = document.getElementById("modalLike");
+
     modalMore.innerHTML = "Подробнее";
+    modalLike.innerHTML = "❤";
+
+    const modalLikeColor = document.getElementById("modalLike");
+    let isColored = false;
+
+    modalLikeColor.addEventListener("click", () => {
+      if (isColored) {
+        modalLikeColor.style.color = "";
+      } else {
+        const colorBtn = "#bb06d3";
+        modalLikeColor.style.color = colorBtn;
+      }
+      isColored = !isColored;
+    });
 
     viewWindow.style.display = "block";
     overlay.style.display = "block";
@@ -150,14 +267,12 @@ async function createProductCards() {
     closeModalButton.addEventListener("click", () => {
       viewWindow.style.display = "none";
       overlay.style.display = "none";
-
     });
   }
 }
 createProductCards();
 
-
-
+// поиск по товарам
 const inputSearch = document.querySelector("input");
 inputSearch.addEventListener("input", search);
 
@@ -166,12 +281,8 @@ function search() {
   const products = document.querySelectorAll(".product__item");
 
   products.forEach((product) => {
-    const searchTitle = product
-      .querySelector(".product-name-span")
-      .textContent.toLowerCase();
-    const searchFirm = product
-      .querySelector(".firm-name")
-      .textContent.toLowerCase();
+    const searchTitle = product.querySelector(".product-name-span").textContent.toLowerCase();
+    const searchFirm = product.querySelector(".firm-name").textContent.toLowerCase();
 
     if (searchTitle.includes(input) || searchFirm.includes(input)) {
       product.style.display = "block";
@@ -180,4 +291,3 @@ function search() {
     }
   });
 }
-
